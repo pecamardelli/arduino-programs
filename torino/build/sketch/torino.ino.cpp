80621c093d1d0d92f20ad8c62f2938e9fbce78a2
Pablo Camardelli <pecamardelli@gmail.com>
2023-12-02 23:38:22 -0300

    Changes made by VS Code

--- torino/build/sketch/torino.ino.cpp
+++ torino/build/sketch/torino.ino.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
-#line 1 "/home/pablin/arduino-programs/torino/torino.ino"
+#line 1 "/home/pablin/arduino_programs/torino/torino.ino"
 #include "header.h"
 
 Commander commander;
 Flowmeter flowmeter;
 
-#line 6 "/home/pablin/arduino-programs/torino/torino.ino"
-void setup();
-#line 13 "/home/pablin/arduino-programs/torino/torino.ino"
-void loop();
-#line 6 "/home/pablin/arduino-programs/torino/torino.ino"
 void setup()
 {
     Serial.begin(9600);
